703b27b9568a37af55432de76d967c33490c2a2e
Sidath Samarasinghe <sidaath@gmail.com>
2024-08-15 20:03:41 +0200

    delete route, default status codes

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, Body, status
 from typing import Annotated
 import api.inventory.inventory  as inventory
 from api.model.inventoryItemModel import InventoryItem
@@ -10,33 +10,33 @@ def test():
     return 'helloooo'
 
 
-@app.get('/inventory')
+@app.get('/inventory', status_code=status.HTTP_200_OK)
 def getInventory():
     print('get inv')
     return inventory.get_all_items()
 
 
-@app.get('/inventory/{item_id}')
+@app.get('/inventory/{item_id}', status_code=status.HTTP_200_OK)
 def get_inv_item(item_id : Annotated[int, Path(title='ID of inventory item to retrieve')]):
     return inventory.get_item_by_id(item_id)
 
 
-@app.get('/inventory/')
+@app.get('/inventory/', status_code=status.HTTP_200_OK)
 def get_items(start: Annotated[int, Query(title='Start index')], 
               end: Annotated[int, Query(title='End index')] 
               ):
     return inventory.get_subset(start, end)
 
 
-@app.post('/inventory')
+@app.post('/inventory', status_code=status.HTTP_201_CREATED)
 def addInvItem(item : InventoryItem) -> str:
     print('add to inv')
     inventory.add_item(item)
     return ("success")
 
 
-@app.get('/remove')
-def remove():
-    print('remove')
-    ret = inventory.remove_item(3)
-    return inventory.get_all_items()
+@app.delete('/inventory/{id}', status_code=status.HTTP_202_ACCEPTED)
+def remove_item(id : int):
+    print(f'remove item %d',id)
+    return {}
+    
